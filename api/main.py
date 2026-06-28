cc5f82be4314fe42835d2e29b1fe2d8d85af3a7f
Shantosh <shantoshs945@gmail.com>
2026-06-28 18:36:29 +0530

    fix: import categories router

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import products, orders, admin, reviews
+from routes import products, orders, admin, reviews, categories 
 
 app = FastAPI()
 
